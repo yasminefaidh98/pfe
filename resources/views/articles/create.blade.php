0438fbb1eef218b9a140ad05c511b5bbc683be45
unknown <fayedhyessmine@yahoo.com>
2021-02-18 11:53:28 +0100

    my first commit

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -27,58 +27,65 @@
     @csrf
   
      <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12">
+
+     <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
-                <strong>Name:</strong>
-                <input type="text" name="reference" class="form-control" placeholder="Name">
+                <strong>id_auto:</strong>
+                <input type="text" name="id_auto" class="form-control" >
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
-                <strong>Detail:</strong>
-                <textarea class="form-control" style="height:150px" name="designation" placeholder="Detail"></textarea>
+                <strong>Reference:</strong>
+                <input type="text" name="reference" class="form-control" >
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
-                <strong>Name:</strong>
+                <strong>Designation:</strong>
+                <textarea class="form-control" style="height:150px" name="designation" ></textarea>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>PA ht:</strong>
                 <input type="text" name="prix_achat_ht" class="form-control" placeholder="Name">
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
-                <strong>Detail:</strong>
-                <textarea class="form-control" style="height:150px" name="famille_id_auto" placeholder="Detail"></textarea>
+                <strong>famille_id_auto:</strong>
+                <textarea class="form-control" style="height:150px" name="famille_id_auto" ></textarea>
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
-                <strong>Name:</strong>
-                <input type="text" name="prix_vente_ht" class="form-control" placeholder="Name">
+                <strong>prix_vente_ht:</strong>
+                <input type="text" name="prix_vente_ht" class="form-control" >
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
-                <strong>Detail:</strong>
-                <textarea class="form-control" style="height:150px" name="stock" placeholder="Detail"></textarea>
+                <strong>stock:</strong>
+                <textarea class="form-control" style="height:150px" name="stock" ></textarea>
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
-                <strong>Name:</strong>
-                <input type="text" name="codebarre" class="form-control" placeholder="Name">
+                <strong>codebarre:</strong>
+                <input type="text" name="codebarre" class="form-control" >
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
-                <strong>Detail:</strong>
-                <textarea class="form-control" style="height:150px" name="prix_vente_ttc" placeholder="Detail"></textarea>
+                <strong>prix_vente_ttc:</strong>
+                <textarea class="form-control" style="height:150px" name="prix_vente_ttc" ></textarea>
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
-                <strong>Detail:</strong>
-                <textarea class="form-control" style="height:150px" name="marque" placeholder="Detail"></textarea>
+                <strong>marque:</strong>
+                <textarea class="form-control" style="height:150px" name="marque" ></textarea>
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12 text-center">
